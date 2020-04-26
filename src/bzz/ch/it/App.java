51a917ba583c2e22e878a3973b59eccf486a672c
sergeantsparkk <sergeantsparkk@gmail.com>
2020-04-26 14:42:51 +0200

    initial commit

---
+++ src/bzz/ch/it/App.java
@@ -0,0 +1,27 @@
+package bzz.ch.it;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JPanel panel1;
+    private JButton button1;
+
+    public App() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"HELLO");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame jframe = new JFrame("Support");
+        jframe.setContentPane(new App().panel1);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.pack();
+        jframe.setVisible(true);
+    }
+}
